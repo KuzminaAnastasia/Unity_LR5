126e74f028273b10549a0f5809974a4f7cb8a246
KuzminaAnastasia <93238968+KuzminaAnastasia@users.noreply.github.com>
2023-12-22 13:58:44 +0500

    init

---
+++ Assets/Standard Assets/Cameras/Scripts/ChangeCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour {
+
+	public GameObject CCTVCamera;
+    public GameObject MainCamera;
+    public bool whichcam = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp("c"))
+        {
+            if (whichcam == false)
+            {
+                whichcam = true;
+            }
+            else if (whichcam == true)
+            {
+                whichcam = false;
+            }
+        }
+
+        if (whichcam == false)
+        {
+            CCTVCamera.active = true;
+            MainCamera.active = false;
+        }
+
+        if (whichcam == true)
+        {
+            CCTVCamera.active = false;
+            MainCamera.active = true;
+        }
+    }
+}
